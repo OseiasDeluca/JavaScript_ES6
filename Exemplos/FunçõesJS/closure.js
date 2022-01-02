42470e16def72fbc4293c36fe060fc718df0c8dc
Oséias Deluca <89862166+OseiasDeluca@users.noreply.github.com>
2022-01-02 20:53:42 -0300

    Ajustes!

--- Exemplos/FunçõesJS/closure.js
+++ Exemplos/FunçõesJS/closure.js
@@ -1,4 +1,5 @@
 // Capacidade de uma função lembrar o ambiente em que foi criada.
+// Legal a título de conhecimento, mas na prática é bom evitar esse tipo e modelo.
 
 function init() { // Essa função init retorna uma outra função
     const exemplo = 'Essa variável' // Cria uma variável exemplo
@@ -31,3 +32,46 @@ initFn3()
 // 1 - O valor da variável exemplo é: Essa variável.
 // 2 - O valor da variável exemplo é: Essa variável.
 // 3 - O valor da variável exemplo é: Essa variável.
+
+
+function init() { // Essa função init retorna uma outra função
+    const exemplo = 'Essa variável' // Cria uma variável exemplo
+
+  // Modelo ideal para códigos longos e antigos, para evitar confusão e saber de onde vem cada variável.
+    return function(exemplo){ // Exemplo de função pura
+        console.log(`1 - O valor da variável exemplo é: ${exemplo}.`) // Todas as funções utilizam o valor de ${exemplo}
+
+        return function(exemplo){
+            console.log(`2 - O valor da variável exemplo é: ${exemplo}`)
+
+            return function(exemplo){
+                console.log(`3 - O valor da variável exemplo é: ${exemplo}`)
+            }
+        }
+    }
+}
+
+// OUTRA FORMA DE EXECUTAR ESSA FUNÇÃO
+
+function init() { // Essa função init retorna uma outra função
+    const exemplo = 'Essa variável' // Cria uma variável exemplo
+
+    return function(){
+        console.log(`1 - O valor da variável exemplo é: ${exemplo}.`) // Todas as funções utilizam o valor de ${exemplo}
+
+        return function(){
+            console.log(`2 - O valor da variável exemplo é: ${exemplo}`)
+
+            return function(){
+                console.log(`3 - O valor da variável exemplo é: ${exemplo}`)
+            }
+        }
+    }
+}
+
+
+// Cadeia de execução
+// NÃO IREMOS USAR, APENAS PARA TÍTULO DE CONHECIMENTO.
+init()()()(); // Executar todas funções em uma linha só.
+
+
