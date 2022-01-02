5eb434ebe8cb250dc757a78db207421db6dadaa6
Oséias Deluca <89862166+OseiasDeluca@users.noreply.github.com>
2022-01-02 20:43:05 -0300

    Update closure.js

--- Exemplos/FunçõesJS/closure.js
+++ Exemplos/FunçõesJS/closure.js
@@ -4,7 +4,7 @@ function init() { // Essa função init retorna uma outra função
     const exemplo = 'Essa variável' // Cria uma variável exemplo
 
     return function(){
-        console.log(`1 - O valor da variável exemplo é: ${exemplo}.`)
+        console.log(`1 - O valor da variável exemplo é: ${exemplo}.`) // Todas as funções utilizam o valor de ${exemplo}
 
         return function(){
             console.log(`2 - O valor da variável exemplo é: ${exemplo}`)
